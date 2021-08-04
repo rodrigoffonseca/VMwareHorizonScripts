9279b1d7ce9c06528f086b23c62c8d647aa584c7
Rodrigo Fonseca <rodrigoffonseca@gmail.com>
2021-08-04 18:06:52 -0300

    Create Available-quota.ps1

---
+++ Available-quota.ps1
@@ -0,0 +1,81 @@
+$location = "eastus"
+$actualquota = Get-AzVMUsage -Location $location 
+foreach ($item in $actualquota) {
+    if ($item.Name.Value -eq "cores"){
+        $temp = ($item.Limit - $item.CurrentValue)
+        Write-host "Checking you quota for vCPU Cores..."
+        If ($temp -gt 32) { 
+            write-host -BackgroundColor Green "Total available vCPU Cores are: "$temp 
+            write-host -BackgroundColor Green "Your Quota for vCPU Core is OK"
+            }
+        else { 
+            write-host -BackgroundColor Red "You need to increase your quota , open a Support Ticket in Azure Portal to request Quota increase" 
+            }
+    }
+    if ($item.Name.Value -eq "virtualMachines"){
+        $temp = ($item.Limit - $item.CurrentValue)
+        Write-host "Checking you quota for Virtual Machines..."
+        If ($temp -gt 16) { 
+            write-host -BackgroundColor Green "Total available VMs are: "$temp 
+            write-host -BackgroundColor Green "Your Quota for Virtual Machines is OK"
+            }
+        else { 
+            write-host -BackgroundColor Red "You need to increase your quota , open a Support Ticket in Azure Portal to request Quota increase" 
+            }
+    }
+    if ($item.Name.Value -eq "standardDSv3Family"){
+        $temp = ($item.Limit - $item.CurrentValue)
+        Write-host "Checking you quota for standardDSv3Family..."
+        If ($temp -gt 4) { 
+            write-host -BackgroundColor Green "Total available standardDSv3Family are: "$temp 
+            write-host -BackgroundColor Green "Your Quota for standardDSv3Family is OK"
+            }
+        else { 
+            write-host -BackgroundColor Red "You need to increase your quota , open a Support Ticket in Azure Portal to request Quota increase" 
+            }
+    }
+    if ($item.Name.Value -eq "standardDSv2Family"){
+        $temp = ($item.Limit - $item.CurrentValue)
+        Write-host "Checking you quota for standardDSv2Family..."
+        If ($temp -gt 4) { 
+            write-host -BackgroundColor Green "Total available standardDSv2Family are: "$temp 
+            write-host -BackgroundColor Green "Your Quota for standardDSv2Family is OK"
+            }
+        else { 
+            write-host -BackgroundColor Red "You need to increase your quota , open a Support Ticket in Azure Portal to request Quota increase" 
+            }
+    }
+    if ($item.Name.Value -eq "standardAv2Family"){
+        $temp = ($item.Limit - $item.CurrentValue)
+        Write-host "Checking you quota for standardAv2Family..."
+        If ($temp -gt 4) { 
+            write-host -BackgroundColor Green "Total available standardAv2Family are: "$temp 
+            write-host -BackgroundColor Green "Your Quota for standardAv2Family is OK"
+            }
+        else { 
+            write-host -BackgroundColor Red "You need to increase your quota , open a Support Ticket in Azure Portal to request Quota increase" 
+            }
+    }
+    if ($item.Name.Value -eq "standardDSv4Family"){
+        $temp = ($item.Limit - $item.CurrentValue)
+        Write-host "Checking you quota for standardDSv4Family..."
+        If ($temp -gt 4) { 
+            write-host -BackgroundColor Green "Total available standardDSv4Family are: "$temp 
+            write-host -BackgroundColor Green "Your Quota for standardDSv4Family is OK"
+            }
+        else { 
+            write-host -BackgroundColor Red "You need to increase your quota , open a Support Ticket in Azure Portal to request Quota increase" 
+            }
+    }
+    if ($item.Name.Value -eq "standardFSv2Family"){
+        $temp = ($item.Limit - $item.CurrentValue)
+        Write-host "Checking you quota for standardFSv2Family..."
+        If ($temp -gt 4) { 
+            write-host -BackgroundColor Green "Total available standardFSv2Family are: "$temp 
+            write-host -BackgroundColor Green "Your Quota for standardFSv2Family is OK"
+            }
+        else { 
+            write-host -BackgroundColor Red "You need to increase your quota , open a Support Ticket in Azure Portal to request Quota increase" 
+            }
+    }
+}
