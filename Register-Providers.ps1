0e0c3eabcc626a5f754c6b7cf036bf462cfcde48
Rodrigo Fonseca <rodrigoffonseca@gmail.com>
2021-08-06 12:11:48 -0300

    Update Register-Providers.ps1

--- Register-Providers.ps1
+++ Register-Providers.ps1
@@ -1,4 +1,6 @@
+#This script will check if all provider required by VMWare Horizon cloud are already registered in your subscription, and will register what is missing.
 $RPList = @("Microsoft.Compute", "microsoft.insights", "Microsoft.Network", "Microsoft.Storage", "Microsoft.KeyVault", "Microsoft.Authorization", "Microsoft.Resources", "Microsoft.ResourceHealth","Microsoft.ResourceGraph","Microsoft.Security","Microsoft.DBforPostgreSQL","Microsoft.Sql")
+#Define your Azure Region / Location where you will deploy Horizon Cloud on Azure
 $location = "eastus"
 foreach ($RP in $RPList)
 {
