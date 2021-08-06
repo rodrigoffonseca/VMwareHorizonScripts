877b63657d14610a221177cddaba9c5878e93cd4
Rodrigo Fonseca <rodrigoffonseca@gmail.com>
2021-08-06 11:40:48 -0300

    Update Create-Network.ps1

--- Create-Network.ps1
+++ Create-Network.ps1
@@ -1,65 +1,47 @@
+#Define your subscription, resource group and Location
 $Sub1           = "Azure Subscription 1"
-$RG1            = "Your-Azure-ResourceGroup-For-Horizon"
-$Location     = "West Europe"
-$VNetName1      = "HSoA-VNET-01"
-$VNetName2      = "HSoA-VNET-Infrastructure"
-$VSubNetName1   = "HSoA-Subnet-01"
-$VSubNetName2   = "Infrastructure"
-$GWSubName1     = "GatewaySubnet"
-$VNet1Prefix    = "192.168.100.0/22"
-$VNet2Prefix    = "192.168.98.0/23"
-$VSubNet1Prefix = "192.168.100.0/24"
-$VSubNet2Prefix = "192.168.98.0/24"
-$GWName1        = "HSoA-VNetGW-01"
-$GWSubPrefix1   = "192.168.99.0/24"
+$RG           = "Your-Azure-ResourceGroup-For-Horizon"
+$Location     = "eastus"
 
-$DNS1           = "192.168.1.13"
+#Define your VNet and Subnet Settings
+$VNetHorizonPrefix = "172.16.0.0/20"
+$VNetHorizonName      = "VNET-Horizon"
+$VSubNetVMName   = "VM-Subnet"
+$VSubNetMGMTName   = "MGMT-Subnet"
+$VSubNetDMZName   = "DMZ-Subnet"
+$SubnetVMPrefix    = "172.16.0.0/22"
+$SubnetMGMTPrefix    = "172.16.4.0/27"
+$SubnetDMZPrefix    = "172.16.4.32/27"
 
+#Define your gateway subnet settings
+$GWSubName     = "GatewaySubnet"
+$GWName        = "HSoA-VNetGW-01"
+$GWSubPrefix   = "172.16.4.64/27"
 $GWIPName1     = "HSoA-VNetGW-01-IP"
 $GWIPconfName1 = "gwipconf1"
-$ConnectionName  = "Site2Site-VPN-VDEMO-Ostrobramska"
-$LNGName       = "Site2SiteVPN-LocalNetGateway"
-$LNGPrefix1   = "192.168.1.0/24"
-$LNGPrefix2   = "192.168.252.0/24"
-$LNGIP         = "83.144.96.2"
+
+#Your Custom DNS (Same from AD DS)
+$DNS1           = "10.0.0.4"
+$DNS2           = "10.0.0.250"
 
 # Connect to your subscription and create a new resource group
 
 $azsubscription = Select-AzSubscription -SubscriptionName $Sub1
-New-AzResourceGroup -Name $RG1 -Location $Location1
+New-AzResourceGroup -Name $RG -Location $Location
 
-# Create virtual networks / spoke vnet for Horizon Service
+# Create virtual networks and Subnets for Horizon Service
 
-$VSubNet1 = New-AzVirtualNetworkSubnetConfig -Name $VSubNetName1 -AddressPrefix $VSubNet1Prefix 
-$vnetHSoA = New-AzVirtualNetwork -Name $VNetName1 -ResourceGroupName $RG1 -Location $Location1 -AddressPrefix $VNet1Prefix -Subnet $VSubNet1 -DnsServer $DNS1
-
-# Create virtual networks / hub vnet for A2S VPN
-
-$VSubNet2 = New-AzVirtualNetworkSubnetConfig -Name $VSubNetName2 -AddressPrefix $VSubNet2Prefix
-$GWSubNet1 = New-AzVirtualNetworkSubnetConfig -Name $GWSubName1 -AddressPrefix $GWSubPrefix1
-$vnetInfra = New-AzVirtualNetwork -Name $VNetName2 -ResourceGroupName $RG1 -Location $Location1 -AddressPrefix $VNet2Prefix -Subnet $VSubNet2,$GWSubNet1 -DnsServer $DNS1
-
-# Peer Infrastructure and VDI networks
-Add-AzVirtualNetworkPeering -Name HSoA-to-Infra -VirtualNetwork $vnetHSoA -RemoteVirtualNetworkId $vnetInfra.Id -AllowForwardedTraffic -AllowGatewayTransit
-Add-AzVirtualNetworkPeering -Name Infra-to-HSoA -VirtualNetwork $vnetInfra -RemoteVirtualNetworkId $vnetHSoA.Id -AllowForwardedTraffic -AllowGatewayTransit
+$VSubNet1 = New-AzVirtualNetworkSubnetConfig -Name $VSubNetVMName -AddressPrefix $SubnetVMPrefix
+$VSubNet2 = New-AzVirtualNetworkSubnetConfig -Name $VSubNetMGMTName -AddressPrefix $SubnetMGMTPrefix
+$VSubNet3 = New-AzVirtualNetworkSubnetConfig -Name $VSubNetDMZName -AddressPrefix $SubnetDMZPrefix
+$GWSubNet1 = New-AzVirtualNetworkSubnetConfig -Name $GWSubName -AddressPrefix $GWSubPrefix
+$vnetHSoA = New-AzVirtualNetwork -Name $VNetName1 -ResourceGroupName $RG -Location $Location -AddressPrefix $VNet1Prefix -Subnet $VSubNet1,$VSubNet2,$VSubNet3,$GWSubNet1 -DnsServer $DNS1,$DNS2
 
 # Create VPN gateway
 
-$gwpip1    = New-AzPublicIpAddress -Name $GWIPName1 -ResourceGroupName $RG1 -Location $Location1 -AllocationMethod Dynamic
-$vnet1     = Get-AzVirtualNetwork -Name $VNetName2 -ResourceGroupName $RG1
-$subnet1   = Get-AzVirtualNetworkSubnetConfig -Name "$GWSubName1" -VirtualNetwork $vnet1
+$gwpip1    = New-AzPublicIpAddress -Name $GWIPName1 -ResourceGroupName $RG -Location $Location -AllocationMethod Dynamic
+$vnet1     = Get-AzVirtualNetwork -Name $VNetHorizonName -ResourceGroupName $RG
+$subnet1   = Get-AzVirtualNetworkSubnetConfig -Name $GWSubName -VirtualNetwork $VNetHorizonName
 $gwipconf1 = New-AzVirtualNetworkGatewayIpConfig -Name $GWIPconfName1 -Subnet $subnet1 -PublicIpAddress $gwpip1
 
-New-AzVirtualNetworkGateway -Name $GWName1 -ResourceGroupName $RG1 -Location $Location1 -IpConfigurations $gwipconf1 -GatewayType Vpn -VpnType RouteBased -GatewaySku VpnGw1
-
-# Create local network gateway - definition of your on-prem network logic
-
-New-AzLocalNetworkGateway -Name $LNGName -ResourceGroupName $RG1 -Location $Location1 -GatewayIpAddress $LNGIP -AddressPrefix $LNGPrefix1,$LNGPrefix2
-
-# Create the S2S VPN connection
-
-$vnet1gw = Get-AzVirtualNetworkGateway -Name $GWName1  -ResourceGroupName $RG1
-$lng5gw  = Get-AzLocalNetworkGateway -Name $LNGName -ResourceGroupName $RG1
-
-New-AzVirtualNetworkGatewayConnection -Name $ConnectionName -ResourceGroupName $RG1 -VirtualNetworkGateway1 $vnet1gw -LocalNetworkGateway2 $lng5gw -Location $Location1 -ConnectionType IPsec -SharedKey 'VMware1!VMware1!' -EnableBGP $False
-
+New-AzVirtualNetworkGateway -Name $GWName1 -ResourceGroupName $RG -Location $Location -IpConfigurations $gwipconf1 -GatewayType Vpn -VpnType RouteBased -GatewaySku VpnGw1
