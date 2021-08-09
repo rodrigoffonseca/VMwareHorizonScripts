ea959b1143627304ce859b70f4aedbf581f80687
Rodrigo Fonseca <rodrigoffonseca@gmail.com>
2021-08-09 14:53:31 -0300

    Update Config-AD.ps1

--- scripts/Config-AD.ps1
+++ scripts/Config-AD.ps1
@@ -1,11 +1,12 @@
 #This script helps you to prepare your On-Premises Active Directory Domain Services for Horizon Cloud Deployment
 #The script will create one Organizational Unit, two users, one group and will delegate appropriate permissions to the group to be able to join HOrizon VMs to the domain
-#Define Variables
+#Define Variables with the appropriate values for your environment
 $OUName = "HOrizon-Azure"
 $BinduserName1 = "binduser1"
 $BinduserName2 = "binduser2"
 $BindPWD = "P@ssword01"
 $bindGroupName = "Horizon-Bind"
+$UPNSuffix = "contoso.com.br"
 #Create New OU and get OU Path
 New-ADOrganizationalUnit -Name $OUName
 $OUpath = Get-ADOrganizationalUnit -Filter 'Name -like $OUName'
@@ -13,8 +14,8 @@ Write-Host "Your OU Path is" $OUpath.DistinguishedName
 #Create Group
 New-ADGroup -Name $bindGroupName -DisplayName $bindGroupName -GroupScope Global -GroupCategory Security -Path $OUpath.DistinguishedName
 #Create two Bind Users
-New-ADUser -Name $BinduserName1 -GivenName $BinduserName1 -SamAccountName $BinduserName1 -Path $OUpath.DistinguishedName -AccountPassword(ConvertTo-SecureString $BindPWD -AsPlainText -Force) -Enabled $true
-New-ADUser -Name $BinduserName2 -GivenName $BinduserName2 -SamAccountName $BinduserName2 -Path $OUpath.DistinguishedName -AccountPassword(ConvertTo-SecureString $BindPWD -AsPlainText -Force) -Enabled $true
+New-ADUser -Name $BinduserName1 -GivenName $BinduserName1 -SamAccountName $BinduserName1 -UserPrincipalName ($BinduserName1+"@"+$UPNSuffix) -Path $OUpath.DistinguishedName -AccountPassword(ConvertTo-SecureString $BindPWD -AsPlainText -Force) -Enabled $true
+New-ADUser -Name $BinduserName2 -GivenName $BinduserName2 -SamAccountName $BinduserName2 -UserPrincipalName ($BinduserName2+"@"+$UPNSuffix) -Path $OUpath.DistinguishedName -AccountPassword(ConvertTo-SecureString $BindPWD -AsPlainText -Force) -Enabled $true
 #Add Bind Users as members of the group
 Add-ADGroupMember -Identity $bindGroupName -Members $BinduserName1, $BinduserName2
 #Set group permission
@@ -23,6 +24,7 @@ Set-Location AD:
 $group = Get-ADGroup -Identity $bindGroupName
 $delegationGroupSID = [System.Security.Principal.SecurityIdentifier] $group.SID
 $delegationGroupACL = Get-Acl -Path $OUpath.DistinguishedName
+
 $aceIdentity = [System.Security.Principal.IdentityReference] $delegationGroupSID
 $aceType = [System.Security.AccessControl.AccessControlType] "Allow"
 
