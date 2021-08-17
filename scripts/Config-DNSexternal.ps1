e2b71f2f974020aabe03bffcca9a7805df89324e
Rodrigo Fonseca <rodrigoffonseca@gmail.com>
2021-08-17 15:46:07 -0300

    Update Config-DNSexternal.ps1

--- scripts/Config-DNSexternal.ps1
+++ scripts/Config-DNSexternal.ps1
@@ -13,6 +13,10 @@ $lb = Get-AzLoadBalancer | where {$_.name -like '*uag*'}
 $temp = Get-AzResource -Id $lb.FrontendIpConfigurations.PublicIpAddress.Id
 $Dnsvalue = Get-AzPublicIpAddress -Name $temp.name -ResourceGroupName $temp.ResourceGroupName
 
+#Display the CNAME value you should use to create the DNS record on your third-party provider
+Write-Host -BackgroundColor Green "Use the following DNS value to create the CNAME record on your External DNS Provider":
+write-host -BackgroundColor Green $Dnsvalue.DnsSettings.Fqdn
+
 #create DNS CNAME Record
 New-AzDnsRecordSet -Name $DNSExteralName -RecordType CNAME -ZoneName $DnsZoneName -ResourceGroupName $rgname -Ttl 3600 -DnsRecords (New-AzDnsRecordConfig -Cname $Dnsvalue.DnsSettings.Fqdn)
 
